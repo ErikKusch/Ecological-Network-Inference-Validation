252cc59e61fa85caf9a7ce714c19661ce7968748
Erik Kusch <erik@i-solution.de>
2024-04-03 11:18:43 +0200

    Put a pbsapply loop in for testing purposes if needed

--- R Scripts/DataSimulations.R
+++ R Scripts/DataSimulations.R
@@ -36,7 +36,7 @@ foreach(ITER = 1:n_runs, .options.snow = opts) %dopar% {
       env.xy = env.xy,
       t_max = t_max,
       t_inter = t_inter,
-      sd = sd,
+      sd = Env_sd,
       migration = migration,
       Effect_Dis = Effect_Dis,
       verbose = verbose
@@ -45,6 +45,39 @@ foreach(ITER = 1:n_runs, .options.snow = opts) %dopar% {
   }
 }
 close(pb)
+
+# SimExecCatch <- pbsapply(1:n_runs, cl = cl, FUN = function(ITER){
+#   if(file.exists(file.path(Dir.Data, paste0(RunName, "_SIM_", ITER, ".RData")))){
+#     1+1
+#   }else{
+#     Simulation_Output <- FUN.SimulationFramework(
+#       seed = ITER,
+#       ## Network Creation
+#       n_spec = n_spec,
+#       NetworkType = NetworkType,
+#       Sparcity = Sparcity,
+#       MaxStrength = MaxStrength,
+#       ## Initial Individual Creation
+#       n_individuals = n_individuals,
+#       n_mode = n_mode,
+#       Env_range = Env_range,
+#       Trait_sd = Trait_sd,
+#       ## Carrying Capacity Creation
+#       k_range = k_range,
+#       ## Simulation Parameters
+#       d0 = d0,
+#       b0 = b0,
+#       env.xy = env.xy,
+#       t_max = t_max,
+#       t_inter = t_inter,
+#       sd = Env_sd,
+#       migration = migration,
+#       Effect_Dis = Effect_Dis,
+#       verbose = verbose
+#     )
+#     save(Simulation_Output, file = file.path(Dir.Data, paste0(RunName, "_SIM_", ITER, ".RData")))
+#   }
+# })
 
 # ### lightening data load by retaining no intermediate simulation steps
 # fs <- list.files(pattern = ".RData", Dir.Data, full.names = TRUE)
